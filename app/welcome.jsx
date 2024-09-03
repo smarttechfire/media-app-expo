3683a5e64d1ee9733a13ba8989e54479d6ddd71a
smarttechfire <educationfire552@gmail.com>
2024-09-03 12:05:24 +0530

    Welcome Screen UI

--- app/welcome.jsx
+++ app/welcome.jsx
@@ -1,15 +1,97 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
-import ScreenWrapper from '~/components/ScreenWrapper'
+import { useRouter } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { View, StyleSheet, Image, Text, Pressable } from 'react-native';
 
-const welcome = () => {
+import Button from '../components/Button';
+import { theme } from '../constants/theme';
+import { wp, hp } from '../helpers/common';
+
+import ScreenWrapper from '~/components/ScreenWrapper';
+
+export default function Welcome() {
+  // const router = useRouter();
   return (
     <ScreenWrapper>
-      <Text>welcome</Text>
+      <StatusBar style="dark" />
+      <View style={styles.container}>
+        {/* welcome image */}
+        <Image
+          style={styles.welcomeImage}
+          resizeMode="contain"
+          source={require('../assets/images/welcome.png')}
+        />
+        {/* title */}
+        <View style={{ gap: 20 }}>
+          <Text style={styles.title}>LinkUp!</Text>
+          <Text style={styles.punchline}>
+            Where every thought finds a home and every image tells a story
+          </Text>
+        </View>
+        {/* footer */}
+        <View style={styles.footer}>
+          <Button
+            title="Getting Started"
+            buttonStyle={{ marginHorizontal: wp(3) }}
+            onPress={() => {}}
+          />
+          {/*  */}
+          <View style={styles.bottomTextContainer}>
+            <Text style={[styles.loginText]}>Already have an account!</Text>
+            <Pressable>
+              <Text
+                style={[
+                  styles.loginText,
+                  { color: theme.colors.primaryDark, fontWeight: theme.fonts.semibold },
+                ]}>
+                Login
+              </Text>
+            </Pressable>
+          </View>
+        </View>
+      </View>
     </ScreenWrapper>
-  )
+  );
 }
 
-export default welcome
-
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    backgroundColor: 'white',
+    paddingHorizontal: wp(4),
+  },
+  welcomeImage: {
+    height: hp(30),
+    width: wp(100),
+    alignSelf: 'center',
+  },
+  title: {
+    color: theme.colors.text,
+    fontSize: hp(4),
+    textAlign: 'center',
+    fontWeight: theme.fonts.extraBold,
+  },
+  punchline: {
+    textAlign: 'center',
+    paddingHorizontal: wp(10),
+    fontSize: hp(2),
+    color: theme.colors.text,
+  },
+  footer: {
+    gap: 30,
+    width: '100%',
+  },
+  bottomTextContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 5,
+  },
+  loginText: {
+    textAlign: 'center',
+    color: theme.colors.text,
+    fontSize: hp(2.3),
+  },
+});
