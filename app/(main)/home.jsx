f265d54dbd0eaa181eb6903a28ea6e3087ba39c8
smarttechfire <educationfire552@gmail.com>
2024-09-03 21:18:07 +0530

    SetUp Session For Authentication

---
+++ app/(main)/home.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import ScreenWrapper from '../../components/ScreenWrapper'
+
+const home = () => {
+  return (
+    <ScreenWrapper>
+      <Text>home</Text>
+    </ScreenWrapper>
+  )
+}
+
+export default home
+
+const styles = StyleSheet.create({})
--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,7 +1,39 @@
+import { useEffect } from 'react';
+import { AuthProvider, useAuth } from '../contaexts/AuthContexr';
 import '../global.css';
 
-import { Stack } from 'expo-router';
+import {  Stack, useRouter } from 'expo-router';
+import { supabase } from '../utils/supabase';
 
-export default function Layout() {
+
+const _layout = () => {
+  return (
+    <AuthProvider>
+      <MainLayout />
+    </AuthProvider>
+  )
+}
+
+
+
+const MainLayout = () => {
+  const { setAuth } = useAuth();
+  const router = useRouter();
+  useEffect(() => {
+    supabase.auth.onAuthStateChange((_event, session) => {
+      console.log('session user: ', session?.user?.id);
+      if (session) {
+        setAuth(session?.user);
+        router.replace('/home');
+        console.log('Email', session?.user?.email);
+      } else {
+        setAuth(null);
+        router.replace('/welcome')
+      }
+    })
+  },[])
+
   return <Stack screenOptions={{ headerShown: false }} />;
 }
+
+export default _layout;
