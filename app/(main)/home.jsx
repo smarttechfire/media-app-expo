15d6c2817c304471b43cbb98bd84cee9bcd43272
Sathish Kumar N <162999084+sathish9117@users.noreply.github.com>
2024-10-08 10:33:04 +0530

    remove logout btn and add home header ui

--- app/(main)/home.jsx
+++ app/(main)/home.jsx
@@ -1,35 +1,104 @@
-import React from 'react';
-import {  Alert, Text } from 'react-native';
+import React, { useEffect } from 'react';
+import { Alert, Pressable, StyleSheet, Text, View } from 'react-native';
 
-import ScreenWrapper from '../../components/ScreenWrapper'
-import { supabase } from '../../utils/supabase';
-import Button from '../../components/Button'
+import Icon from '../../assets/icons';
+import ScreenWrapper from '../../components/ScreenWrapper';
+import { theme } from '../../constants/theme';
 import { useAuth } from '../../contexts/AuthProvider';
+import { hp, wp } from '../../helpers/common';
+import { supabase } from '../../utils/supabase';
 
 export default function Home() {
+  const { user, setAuth } = useAuth();
+  console.log('user', user.id);
+  const { session } = useAuth();
 
-  const {user} = useAuth();
+  useEffect(() => {
+    if (session) console.log('My Login success');
+  }, [session]);
 
-  // console.log("user: ", user);
-  
-
-
-  const onLogout = async () =>{
+  const onLogout = async () => {
     const { error } = await supabase.auth.signOut();
-  if (error) {
+    if (error) {
       Alert.alert('Sign Out', 'Error signing out!');
-  }else{
+    } else {
       console.log('logout done');
-    
-  }
-  }
+    }
+  };
   return (
     <ScreenWrapper>
-      <Text>Hme</Text>
-      <Button title="logout" onPress={onLogout} />
-
-     </ScreenWrapper>
+      {/* Header */}
+      <View style={styles.header}>
+        <Text style={styles.title}>LinkUp</Text>
+        <View style={styles.icons}>
+          <Pressable>
+            <Icon name="heart" size={hp(3.2)} strokeWidth={2} color={theme.colors.text} />
+          </Pressable>
+          <Pressable>
+            <Icon name="plus" size={hp(3.2)} strokeWidth={2} color={theme.colors.text} />
+          </Pressable>
+          <Pressable>
+            <Icon name="user" size={hp(3.2)} strokeWidth={2} color={theme.colors.text} />
+          </Pressable>
+        </View>
+      </View>
+    </ScreenWrapper>
   );
 }
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 10,
+    marginHorizontal: wp(4),
+  },
+  title: {
+    color: theme.colors.text,
+    fontSize: hp(3.2),
+    fontWeight: theme.fonts.bold,
+  },
+  avatarImage: {
+    height: hp(4.3),
+    width: hp(4.3),
+    borderRadius: theme.radius.sm,
+    borderCurve: 'continuous',
+    borderColor: theme.colors.gray,
+    borderWidth: 3,
+  },
+  icons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 18,
+  },
+  listStyle: {
+    paddingTop: 20,
+    paddingHorizontal: wp(4),
+  },
+  noPosts: {
+    fontSize: hp(2),
+    textAlign: 'center',
+    color: theme.colors.text,
+  },
+  pill: {
+    position: 'absolute',
+    right: -10,
+    top: -4,
+    height: hp(2.2),
+    width: hp(2.2),
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 20,
+    backgroundColor: theme.colors.roseLight,
+  },
+  pillText: {
+    color: 'white',
+    fontSize: hp(1.2),
+    fontWeight: theme.fonts.bold,
+  },
+});
