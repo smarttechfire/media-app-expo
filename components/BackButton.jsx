2321ba0dee00ec2906a8275aa3e77b703c802543
Sathish Kumar N <162999084+sathish9117@users.noreply.github.com>
2024-10-08 23:06:36 +0530

    fix

--- components/BackButton.jsx
+++ components/BackButton.jsx
@@ -1,21 +1,22 @@
-import { Pressable, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import Icon from '../assets/icons'
-import { theme } from '../constants/theme'
+import React from 'react';
+import { Pressable, StyleSheet } from 'react-native';
 
-export default function BackButton({size=26,router}) {
+import Icon from '../assets/icons';
+import { theme } from '../constants/theme';
+
+export default function BackButton({ size = 26, router }) {
   return (
     <Pressable onPress={() => router.back()} style={styles.button}>
       <Icon name="arrowLeft" strokeWidth={2.5} size={size} color={theme.colors.text} />
     </Pressable>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    button:{
-        alignSelf: 'flex-start',
-        padding: 5,
-        borderRadius: theme.radius.sm,
-        backgroundColor: 'rgba(0,0,0,0.07)'
-    }
-})
+  button: {
+    alignSelf: 'flex-start',
+    padding: 5,
+    borderRadius: theme.radius.sm,
+    backgroundColor: 'rgba(0,0,0,0.07)',
+  },
+});
