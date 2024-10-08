890281df59ae7d835fa5e4fdb42295e220fe6309
Sathish Kumar N <162999084+sathish9117@users.noreply.github.com>
2024-10-08 23:09:14 +0530

    Add Notifiactions and NewPost components to main app directory

---
+++ app/(main)/notifiactions.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import ScreenWrapper from '../../components/ScreenWrapper';
+
+const Notifiactions = () => {
+  return (
+    <ScreenWrapper>
+      <Text>Notif</Text>
+    </ScreenWrapper>
+  );
+};
+
+export default Notifiactions;
+
+const styles = StyleSheet.create({});
